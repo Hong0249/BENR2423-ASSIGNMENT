acefc4c8ed5ec5f17cc9dc4575473e6a1a0a6cd3
SyazaniSy <101816584+SyazaniSy@users.noreply.github.com>
2022-05-08 17:09:54 +0800

    Add files via upload

--- user.js
+++ user.js
@@ -1,35 +1,112 @@
-let users;
+const bcrypt = require("bcrypt");
+const { mongoConnection } = require("./connection");
 
-class User {
-	static async injectDB(conn) {
-		users = await conn.db("my-database-name").collection("users")
-	}
+/**
+ * @addUser
+ */
 
-	static async Register(name, age, gender, room_number, contact_number, mysj_status, email, password) {
-		// TODO: Register name, age, gender, room_number, contact_number, mysj_status, email exists
+function addUser(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // check if user does not exists
+      let checkUserData = await checkIfUserExists({ email: userData.email });
+      if (checkUserData.data && checkUserData.data.length > 0) {
+        // user already exists, send response
+        return resolve({
+          error: true,
+          message: "User already exists with this credentials. Please login",
+          data: [],
+        });
+      }
+      // generate password hash
+      let passwordHash = await bcrypt.hash(userData.password, 15);
+      userData.password = passwordHash;
 
-		// TODO: Hash password
-
-		// TODO: Save BookingandReservation to database
-	}
-
-	static async BookingandReservation(name, email, password, time_slot, number_of_visitors) {
-		// TODO: Check if name, email, time_slot, number_of_viitors exists
-
-		// TODO: Hash password
-
-		// TODO: Save BookingandReservation to database
-        return
-	}
-
-	static async Facilities_info(facilities_manager_name, location, max_num_of_visitor, facilities_manager_contact_number, email, password) {
-		// TODO: Check if facilitiesmanagername, location, max_num_of_visitor, facilities_manager_contact_number, email exists
-
-		// TODO: Validate password
-
-		// TODO: Save Facilities_info to database
-        return
-	}
+      // add new user
+      mongoConnection
+        .collection("users")
+        .insertOne(userData, async (err, results) => {
+          if (err) {
+            console.log(err);
+            throw new Error(err);
+          }
+          //return data
+          resolve({
+            error: false,
+            data: results.ops[0],
+          });
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
-module.exports = User;
+/**
+ * @verifyUser
+ * @param {*} userData
+ */
+
+function verifyUser(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userDatafromDb = await checkIfUserExists({ email: userData.email });
+      if (userDatafromDb.data && userDatafromDb.data.length > 0) {
+        // user already exists, verify the password
+        let passwordVerification = await bcrypt.compare(
+          userData.password,
+          userDatafromDb.data[0].password
+        );
+        if (!passwordVerification) {
+          // password mismatch
+          return resolve({
+            error: true,
+            message: "Invalid email or password",
+            data: [],
+          });
+        }
+        // password verified
+        return resolve({ error: false, data: userDatafromDb.data[0] });
+      } else {
+        return resolve({
+          error: true,
+          message:
+            "There is no user exists with this credentials. Please create a new account.",
+          data: [],
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      reject(e);
+    }
+  });
+}
+
+/**
+ * @checkIfUserExists
+ */
+
+function checkIfUserExists(userData) {
+  return new Promise((resolve, reject) => {
+    try {
+      // check if user exists
+      mongoConnection
+        .collection("users")
+        .find({ email: userData.email })
+        .toArray((err, results) => {
+          if (err) {
+            console.log(err);
+            throw new Error(err);
+          }
+          resolve({ error: false, data: results });
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+module.exports = {
+  addUser: addUser,
+  verifyUser: verifyUser,
+};
