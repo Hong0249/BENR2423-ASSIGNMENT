892e5502e7d0a8a29055d75af46421e0da84e9f9
Yak Xuan Hong <b022010002@student.utem.edu.my>
2022-05-25 23:08:25 +0800

    Adding Swagger Documentation

--- main.js
+++ main.js
@@ -17,23 +17,76 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'FVMS API',
+			version: '1.0.0',
+		},
+	},
+	apis: ['./main.js'], // files containing annotations as above
+};
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
+	res.send('Welcome To FVMS API')
 })
 
-app.get('/hello', (req, res) => {
-	res.send('Hello BENR2423')
-})
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id: 
+ *           type: string
+ *         username: 
+ *           type: string
+ *         password: 
+ *           type: string
+ */
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     description: User Login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema: 
+ *             type: object
+ *             properties:
+ *               username: 
+ *                 type: string
+ *               password: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User Login Successful!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Invalid username or password
+ */
 app.post('/login', async (req, res) => {
 	console.log(req.body);
 
 	const user = await User.login(req.body.username, req.body.password);
-	if (user != null ) {
-		console.log("Login successful");
+	if (user != null) {
+		console.log("User Login Successful!");
 		res.status(200).json({
 			_id: user[0]._id,	
 			username: user[0].username,
@@ -64,6 +117,26 @@ app.post('/register', async (req, res) => {
 
 })
 
+/**
+ * @swagger
+ * /visitor/{id}:
+ *   get:
+ *     description: Get visitor by id
+ *     parameters:
+ *       - in: path
+ *         name: id 
+ *         schema: 
+ *           type: string
+ *         required: true
+ *         description: visitor id
+ */
+ app.get('/visitor/:id', async (req, res) => {
+	console.log(req.params.id);
+	//Still Working on this
+
+	res.status(200).json({})
+})
+
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+	console.log(`VMS REST API listening on port ${port}`)
 })
